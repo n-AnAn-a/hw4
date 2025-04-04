8577bd39f009e008c05c71566b936b95e50e8ca7
YiAn Xu <yianxu@usc.edu>
2025-04-04 05:11:57 +0000

    equalpathsDone

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -9,10 +9,30 @@ using namespace std;
 
 // You may add any prototypes of helper functions here
 
+bool helper (Node* root, int* leafHeight, int currentHeight) {
+  if (root == nullptr) {
+    return true;
+  }
+
+  if (root->left == nullptr && root->right == nullptr) {
+    if (*leafHeight == 0) {
+      *leafHeight = currentHeight;
+      return true;
+    }
+    if (currentHeight == *leafHeight) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  return helper(root->left, leafHeight, currentHeight + 1) && helper(root->right, leafHeight, currentHeight + 1);
+}
 
 bool equalPaths(Node * root)
 {
     // Add your code below
-
+  int leafHeight = 0;
+  return helper(root, &leafHeight, 0);
 }
 
